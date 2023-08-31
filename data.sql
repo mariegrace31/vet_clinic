b6d1e1effa9af376d58088a23580ba3342f6a95b
mariegrace31 <mariegracebmg@gmail.com>
2023-08-31 13:46:41 +0200

    Update the data.sql file

--- data.sql
+++ data.sql
@@ -1,5 +1,12 @@
 /* Populate database with sample data. */
+ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+ VALUES (1, 'Agumon', DATE '2020-02-03', 0, true, 10.23);
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+ VALUES (2, 'Gabumon', DATE '2018-11-15', 2, true, 8);
+
+ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+ VALUES (3, 'Pikachu', DATE '2021-01-7', 1, false, 15.04);
+
+ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+ VALUES (4, 'Devimon', DATE '2017-05-12', 5, true, 11);
