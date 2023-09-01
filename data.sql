92b06c58bc1501d07aa1c1ca039e9918bb951eb6
mariegrace31 <mariegracebmg@gmail.com>
2023-09-01 17:56:01 +0200

    Update the data.sql file

--- data.sql
+++ data.sql
@@ -1,35 +1,35 @@
 /* Populate database with sample data. */
  INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
- VALUES (1, 'Agumon', DATE '2020-02-03', 0, true, 10.23);
+ VALUES ('Agumon', DATE '2020-02-03', 0, true, 10.23);
 
  INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
- VALUES (2, 'Gabumon', DATE '2018-11-15', 2, true, 8);
+ VALUES ('Gabumon', DATE '2018-11-15', 2, true, 8);
 
  INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
- VALUES (3, 'Pikachu', DATE '2021-01-7', 1, false, 15.04);
+ VALUES ('Pikachu', DATE '2021-01-7', 1, false, 15.04);
 
  INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
- VALUES (4, 'Devimon', DATE '2017-05-12', 5, true, 11);
+ VALUES ('Devimon', DATE '2017-05-12', 5, true, 11);
 
 /* New data */
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (5, 'Charmander', DATE '2020-02-08', 0, false, -11);
+VALUES ('Charmander', DATE '2020-02-08', 0, false, -11);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (6, 'Plantmon', DATE '2021-11-15', 2, true, -5.7);
+VALUES ('Plantmon', DATE '2021-11-15', 2, true, -5.7);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (7, 'Squirtle', DATE '1993-04-02', 3, false, -12.13);
+VALUES ('Squirtle', DATE '1993-04-02', 3, false, -12.13);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (8, 'Angemon', DATE '2005-06-12', 1, true, -45);
+VALUES ('Angemon', DATE '2005-06-12', 1, true, -45);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (9, 'Boarmon', DATE '2005-06-07', 7, true, 20.4);
+VALUES ('Boarmon', DATE '2005-06-07', 7, true, 20.4);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (10, 'Blossom', DATE '1998-10-13', 3, true, 17);
+VALUES ('Blossom', DATE '1998-10-13', 3, true, 17);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (11, 'Ditto', DATE '2022-05-14', 4, true, 22);
+VALUES ('Ditto', DATE '2022-05-14', 4, true, 22);
