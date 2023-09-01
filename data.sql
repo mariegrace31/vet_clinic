9fb158142a863015dc0af21d898c6c3c5f9394cc
mariegrace31 <mariegracebmg@gmail.com>
2023-09-01 12:42:24 +0200

    Add new data in the data.sql file

--- data.sql
+++ data.sql
@@ -10,3 +10,26 @@
 
  INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
  VALUES (4, 'Devimon', DATE '2017-05-12', 5, true, 11);
+
+/* New data */
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (5, 'Charmander', DATE '2020-02-08', 0, false, -11);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (6, 'Plantmon', DATE '2021-11-15', 2, true, -5.7);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (7, 'Squirtle', DATE '1993-04-02', 3, false, -12.13);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (8, 'Angemon', DATE '2005-06-12', 1, true, -45);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (9, 'Boarmon', DATE '2005-06-07', 7, true, 20.4);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (10, 'Blossom', DATE '1998-10-13', 3, true, 17);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (11, 'Ditto', DATE '2022-05-14', 4, true, 22);
